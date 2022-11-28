e2891ff1420079b1e016ec193dc147632b2e8b11
tithiSynopsys <105774225+tithiSynopsys@users.noreply.github.com>
2022-11-28 13:25:18 +0530

    bd push

--- untitled/src/HelloWord.java
+++ untitled/src/HelloWord.java
@@ -2,5 +2,21 @@ public class HelloWord {
     public static void main (String args[])
     {
         System.out.println ("hello world");
+        vuner;
+    }
+
+    public void vuner(){
+        String firstname = "firstname";
+        String lastname = lastname";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection( "url", "aa", "password);
+        String query = "SELECT id, firstname, lastname FROM authors WHERE firstname = ? and lastname = ?";
+        PreparedStatement pstmt = connection.prepareStatement( query );
+        pstmt.setString( 1, firstname );
+        pstmt.setString( 2, lastname );
+        try
+        {
+            ResultSet results = pstmt.execute( );
+        }
     }
 }
